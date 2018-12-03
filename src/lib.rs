aeedcc1af8c989b13d23437dfe8e6a94f1d3ac93
phayes <patrick.d.hayes@gmail.com>
2018-12-02 20:24:39 -0800

    Using serde to build Json types

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,13 @@
-extern crate cfg_if;
-extern crate wasm_bindgen;
-
 mod utils;
 
 use cfg_if::cfg_if;
+use serde_derive::Serialize;
+use tallyman_rs::plurality::PluralityTally as RustPluralityTally;
 use wasm_bindgen::prelude::*;
+use wasm_bindgen::JsValue;
 
 cfg_if! {
-    // When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
-    // allocator.
+    // When the `wee_alloc` feature is enabled, use `wee_alloc` as the global allocator.
     if #[cfg(feature = "wee_alloc")] {
         extern crate wee_alloc;
         #[global_allocator]
@@ -17,11 +16,41 @@ cfg_if! {
 }
 
 #[wasm_bindgen]
-extern {
-    fn alert(s: &str);
+extern "C" {
+  fn alert(s: &str);
 }
 
 #[wasm_bindgen]
 pub fn greet() {
-    alert("Hello, tallyman!");
+  alert("Hello, tallyman!");
+}
+
+#[wasm_bindgen]
+pub struct PluralityTally {
+  tally: RustPluralityTally<String, u64>,
+}
+
+#[wasm_bindgen]
+impl PluralityTally {
+  pub fn new(num_winners: u32) -> PluralityTally {
+    return PluralityTally {
+      tally: RustPluralityTally::new(num_winners),
+    };
+  }
+
+  pub fn add(&mut self, vote: String) {
+    self.tally.add(vote);
+  }
+
+  pub fn candidates(&self) -> JsValue {
+    JsValue::from_serde(&self.tally.candidates()).unwrap()
+  }
+
+  pub fn totals(&self) -> JsValue {
+    JsValue::from_serde(&self.tally.totals()).unwrap()
+  }
+
+  pub fn ranked(&self) -> JsValue {
+    JsValue::from_serde(&self.tally.ranked()).unwrap()
+  }
 }
